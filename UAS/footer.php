3894b56f2fb7fb61c6b0291d1f37b46039885eaf
RZTHUB/WEB2 <79934802+indrajit666@users.noreply.github.com>
2021-04-09 03:02:46 +0700

    Create footer.php

---
+++ UAS/footer.php
@@ -0,0 +1,2 @@
+<div class="pull-right hidden-xs"><b>Version</b> 1.6.0</div>
+<strong>Copyright &copy; <?php Echo date("Y"); ?> <a href="https://Zearch-Soft.Net">Zetix Arch-Soft LLC.</a></strong> All Rights Reserved.
