37ea92031768a32e51de77a42850ef352e74ee46
RZTHUB/WEB2 <79934802+indrajit666@users.noreply.github.com>
2021-04-09 03:01:42 +0700

    Create Dashboard.php

---
+++ UAS/Dashboard.php
@@ -0,0 +1,30 @@
+<!-- Info boxes -->
+<div class="row">
+<?php include "Account/Info.php"; ?>
+	<div class="col-md-9">
+		<div class="box box-default box-solid">
+			<div class="box-header with-border">
+				<h3 class="box-title">Dashboard</h3>
+				<div class="box-tools pull-right">
+					<button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+					<button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+				</div>
+			</div>
+			<!-- Box Header -->
+			<div class="box-body">
+			Body
+			</div>
+			<!-- /Box Header -->
+			
+			<!-- /Box Body -->
+			<div class="box-footer">
+			Footer
+			</div>
+			<!-- /Box Footer -->
+		</div>
+		<!-- /Box -->
+	</div>
+	<!-- /Col -->
+	
+</div>
+<!-- /Row -->
